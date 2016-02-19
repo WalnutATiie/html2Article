9940eaa2bc3bacd6631cd7a69cfcf22aaf510fec
WalnutATiie <lkkmukevin@163.com>
2016-02-19 03:29:39 -0500

    add another way

--- html2Article.py
+++ html2Article.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import sys
+import re
 from htmlGetter import HtmlGetter 
 from pattern.web import plaintext
 class Html2Article:
     def __init__(self):
         pass
     def html_2_Plaintext(self,html):
+        #text = re.sub(r"(?is)<.*?>","",html)
         text = plaintext(html,keep=[],replace='',linebreaks=1, indentation=False)
         print text
 if __name__ =="__main__":
